d76e615af425efaecdfeefd073a15edfb419e005
adwaid20 <adwaidajayantc22@gmail.com>
2026-02-21 09:44:19 +0530

    validation work ongoing

--- makbig/adminpanel/middleware.py
+++ makbig/adminpanel/middleware.py
@@ -4,9 +4,9 @@ class NoCacheMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
-
-        response["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
-        response["Pragma"] = "no-cache"
-        response["Expires"] = "0"
+        if request.user.is_authenticated:
+            response["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+            response["Pragma"] = "no-cache"
+            response["Expires"] = "0"
 
         return response
